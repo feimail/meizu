0da47f04da55e3eb39cbf96e00f4f3f78f566460
feimail <xf@feimail.cn>
2018-03-28 01:34:27 +0800

    添加软删除，便于查看用户修改密码情况

--- app/Model/PasswordResets.php
+++ app/Model/PasswordResets.php
@@ -3,8 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PasswordResets extends Model
 {
+	//添加软删除
+	use SoftDeletes;
+	protected $dates = ['deleted_at'];
     protected $table = 'password_resets';
 }
