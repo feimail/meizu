e9e126d4afff2a647e685d87265814a3688e6ffe
feimail <xf@feimail.cn>
2018-04-01 23:41:00 +0800

    生成订单号，仿淘宝订单号生成

--- app/Common/func.php
+++ app/Common/func.php
@@ -126,4 +126,39 @@
 
 		return $getPasswordRandStr;
 	}
+
+	/**
+	*  生成订单号,
+	*
+	*/
+	function getOrderNumber(){
+		$time = time();
+		$rand = rand(10,99);
+		$id = 100000 + session('uid');
+		$orderId = $time.$rand.$id;
+		return $orderId;
+	}
+
+	/**
+	*获取guid
+	*
+	*/
+	function guid() {    
+    if (function_exists('com_create_guid')) {        
+        return com_create_guid();    
+    } else {     
+        mt_srand((double)microtime()*10000);
+        $charid = strtoupper(md5(uniqid(rand(), true))); 
+        $hyphen = chr(45);        
+        $uuid   = chr(123)            
+                 .substr($charid, 0, 8).$hyphen               
+                 .substr($charid, 8, 4).$hyphen            
+                 .substr($charid,12, 4).$hyphen            
+                 .substr($charid,16, 4).$hyphen            
+                 .substr($charid,20,12)            
+                 .chr(125);
+        return $uuid;    
+    }
+}
+
 	
